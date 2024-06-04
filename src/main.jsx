6f2536e7c4f466a97373334597ddb81fece0bf04
Mr-Kasa <156632133+Mr-Kasa@users.noreply.github.com>
2024-06-04 12:44:12 +0200

    imported browser route

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter } from "react-router-dom"
+
+function App() {
+  return (
+    <h1>Hello world!</h1>
+  )
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-)
+  </BrowserRouter>
+);
