7338662ef4a52d0e3f5958cb035f611d9ef664d0
Mr-Kasa <156632133+Mr-Kasa@users.noreply.github.com>
2024-06-09 11:45:40 +0200

    Used nesting for Routes

--- src/App.jsx
+++ src/App.jsx
@@ -14,18 +14,20 @@ import Reviews from "../src/pages/host/Reviews"
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/vans" element={<Vans />} />
-          <Route path="/vans/:id" element={<VanDetail />} />
-    <Route path="/host" element={<Dashboard />}>
-    <Route path="/host/income" element={<Income />} />
-    <Route path="/host/reviews" element={<Reviews />} />
-  </Route>
-</Route>
-</Routes>
-</BrowserRouter>)
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="vans" element={<Vans />} />
+        <Route path="vans/:id" element={<VanDetail />} />
+        
+        <Route path="host" element={<HostLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="income" element={<Income />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+      </Route>
+    </Routes>
+  </BrowserRouter>)
     
 }
