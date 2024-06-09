6630767ea482124f5ec8ce3b448e8bbe3478c91f
Mr-Kasa <156632133+Mr-Kasa@users.noreply.github.com>
2024-06-09 11:29:21 +0200

    Created  pages for dashboard, Reviews and Income.

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,28 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans from "./pages/Vans"
-import VanDetails from "./pages/VanDetail"
+import VanDetail from "./pages/VanDetail"
 import Layout from '../components/Layout';
+import Dashboard from "../src/pages/host/Dashboard"
+import Income from "../src/pages/host/Income"
+import Reviews from "../src/pages/host/Reviews"
 
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetails />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>)
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+    <Route path="/host" element={<Dashboard />}>
+    <Route path="/host/income" element={<Income />} />
+    <Route path="/host/reviews" element={<Reviews />} />
+  </Route>
+</Route>
+</Routes>
+</BrowserRouter>)
     
 }
