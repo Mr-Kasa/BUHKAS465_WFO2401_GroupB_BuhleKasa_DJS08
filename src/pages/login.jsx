de27586184a47a5c66e5fd782f4724fd1e669cb4
Mr-Kasa <156632133+Mr-Kasa@users.noreply.github.com>
2024-06-10 11:27:59 +0200

    fixed authentication bug

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,27 +1,30 @@
 import React from "react"
-import { useNavigate, useLocation } from "react-router-dom"
-
-/**
- * Challenge: hook up our form so it (halfway) works.
- * 
- * 1. Pull in the `loginUser` function from the api.js file
- * 2. Call loginUser when the form is submitted and log the
- *    data that comes back. Use "b@b.com" as the username and
- *    "p123" as the password.
- * 
- *    NOTE: loginUser returns a promise, so you'll need
- *    a .then(data => {...}) to access the data, or use
- *    a separate aync function defined inside handleSubmit
- * 3. TBA
- */ 
+import { useLocation, useNavigate } from "react-router-dom"
+import { loginUser } from "../api"
 
 export default function Login() {
     const [loginFormData, setLoginFormData] = React.useState({ email: "", password: "" })
+    const [status, setStatus] = React.useState("idle")
+    const [error, setError] = React.useState(null)
+
     const location = useLocation()
-    
+    const navigate = useNavigate()
+
     function handleSubmit(e) {
         e.preventDefault()
-        console.log(loginFormData)
+        setStatus("submitting")
+        loginUser(loginFormData)
+            .then(data => {
+                setError(null)
+                localStorage.setItem("loggedin", true)
+                navigate("/host", { replace: true })
+            })
+            .catch(err => {
+                setError(err)
+            })
+            .finally(() => {
+                setStatus("idle")
+            })
     }
 
     function handleChange(e) {
@@ -35,10 +38,15 @@ export default function Login() {
     return (
         <div className="login-container">
             {
-                location.state?.message &&
-                <h3 className="login-first">{location.state.message}</h3>
+                location.state ?.message &&
+                    <h3 className="login-error">{location.state.message}</h3>
             }
             <h1>Sign in to your account</h1>
+            {
+                error ?.message &&
+                    <h3 className="login-error">{error.message}</h3>
+            }
+
             <form onSubmit={handleSubmit} className="login-form">
                 <input
                     name="email"
@@ -54,7 +62,14 @@ export default function Login() {
                     placeholder="Password"
                     value={loginFormData.password}
                 />
-                <button>Log in</button>
+                <button
+                    disabled={status === "submitting"}
+                >
+                    {status === "submitting"
+                        ? "Logging in..."
+                        : "Log in"
+                    }
+                </button>
             </form>
         </div>
     )
