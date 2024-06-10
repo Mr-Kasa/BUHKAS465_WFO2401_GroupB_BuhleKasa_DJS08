52da622f1f9e8b3c36ddd84f8c529c2ce0bca382
Mr-Kasa <156632133+Mr-Kasa@users.noreply.github.com>
2024-06-10 11:15:48 +0200

    Added error handling

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,12 @@ import Reviews from "../src/pages/host/Reviews"
 import HostVans from "../src/pages/host/HostVans"
 import HostVanDetail from "../src/pages/host/HostVanDetail"
 import NotFound from './pages/NotFoundPage';
+import AuthRequired from "../components/AuthRequired"
 
 
-function App() {
+export default function App() {
+
+
   return (
     <BrowserRouter>
       <Routes>
@@ -23,19 +26,26 @@ function App() {
           <Route path="about" element={<About />} />
           <Route path="vans" element={<Vans />} />
           <Route path="vans/:id" element={<VanDetail />} />
-          
-          <Route path="host" element={<HostLayout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="income" element={<Income />} />
-            <Route path="reviews" element={<Reviews />} />
-            <Route path="vans" element={<HostVans />} />
-            <Route path="vans/:id" element={<HostVanDetail />}>
-              <Route index element={<HostVanInfo />} />
-              <Route path="pricing" element={<HostVanPricing />} />
-              <Route path="photos" element={<HostVanPhotos />} />
+          <Route
+            path="login"
+            element={<Login />}
+          />
+
+          <Route element={<AuthRequired />}>
+            <Route path="host" element={<HostLayout />}>
+              <Route index element={<Dashboard />} />
+              <Route path="income" element={<Income />} />
+              <Route path="reviews" element={<Reviews />} />
+              <Route path="vans" element={<HostVans />} />
+              <Route path="vans/:id" element={<HostVanDetail />}>
+                <Route index element={<HostVanInfo />} />
+                <Route path="pricing" element={<HostVanPricing />} />
+                <Route path="photos" element={<HostVanPhotos />} />
+              </Route>
             </Route>
           </Route>
-          <Route path="*" element={<NotFound />}/>
+
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
