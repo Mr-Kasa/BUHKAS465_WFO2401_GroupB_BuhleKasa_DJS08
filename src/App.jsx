61e6cfc627aba23ccd2c6ea2503a10b4ad4de42a
Mr-Kasa <156632133+Mr-Kasa@users.noreply.github.com>
2024-06-10 10:14:26 +0200

    Added not found page for broken links

--- src/App.jsx
+++ src/App.jsx
@@ -11,31 +11,33 @@ import Income from "../src/pages/host/Income"
 import Reviews from "../src/pages/host/Reviews"
 import HostVans from "../src/pages/host/HostVans"
 import HostVanDetail from "../src/pages/host/HostVanDetail"
+import NotFound from './pages/NotFoundPage';
 
 
-export default function App() {
+function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="vans" element={<Vans />} />
-        <Route path="vans/:id" element={<VanDetail />} />
-        
-        <Route path="host" element={<HostLayout />}>
-          <Route index element={<Dashboard />} />
-          <Route path="income" element={<Income />} />
-          <Route path="reviews" element={<Reviews />} />
-          <Route path="vans" element={<HostVans />} />
-          <Route path="vans/:id" element={<HostVanDetail />}>
-            <Route index element={<HostVanInfo />} />
-            <Route path="pricing" element={<HostVanPricing />} />
-            <Route path="photos" element={<HostVanPhotos />} />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
+          
+          <Route path="host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
+            <Route path="vans" element={<HostVans />} />
+            <Route path="vans/:id" element={<HostVanDetail />}>
+              <Route index element={<HostVanInfo />} />
+              <Route path="pricing" element={<HostVanPricing />} />
+              <Route path="photos" element={<HostVanPhotos />} />
+            </Route>
           </Route>
+          <Route path="*" element={<NotFound />}/>
         </Route>
-      </Route>
-    </Routes>
-  </BrowserRouter>
-)
+      </Routes>
+    </BrowserRouter>
+  )
 }
